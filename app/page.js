427e011256d55cb4380f640fa613916896e90404
Elmorsy11 <egypt.team.dev>
2024-04-01 12:35:20 +0200

    stage

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ul>
-        <li> vercel env {process.env.VERCEL_ENV} </li>
+        <li> vercel env {process.env.VERCEL_ENV} stage</li>
         <li> env var {process.env.ENV_VAR} </li>
       </ul>
     </main>
